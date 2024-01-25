1745f8438bb4e8f4fbade2f15fd7f519f82d22b3
tral-tral <92859263+tral-tral@users.noreply.github.com>
2024-01-25 20:52:12 +0900

    Update image.php

--- JorangeJorms/core/form/render/classes/fields/image.php
+++ JorangeJorms/core/form/render/classes/fields/image.php
@@ -47,7 +47,7 @@ if( $value ){
     <div class="jorms-form-upload-image-container dropzone" id="<?php echo $field_id ?>" data-amount="<?php echo $original_amount; ?>" data-nonce="<?php echo $nonce; ?>" data-default="<?php echo $value_json ?>">
         <div class="dz-message"><button class="dz-button" type="button"><svg xmlns="http://www.w3.org/2000/svg" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon-upload">
                       <path d="M12 16v-4m0 0-3 3m3-3 3 3M4 9a8 8 0 0 1 8-8c.5 0 1 .04 1.5.12A5.5 5.5 0 0 1 17 9h2a7.98 7.98 0 0 0-7.5-6.97A7.98 7.98 0 0 0 4 9zm0 0v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9"/>
-                    </svg><div class="jorms-form-instructions">Drag to or click here</div>
+                    </svg><div class="jorms-form-in-instructions"><?php _e('Drag to or click here','jorms') ?></div>
             </button></div>
         <?php
 
@@ -55,7 +55,7 @@ if( $value ){
             echo '<div class="empty dz-button"></div>';
         } ?>
     </div>
-    <span class="jorms-form-instructions">Upload PNG/JPG. Max file size 2MB.</span>
+    <span class="jorms-form-instructions"><?php _e('Upload PNG/JPG. Max file size 2MB.','jorms') ?></span>
     <div class="error-message"></div>
 </div>
 
